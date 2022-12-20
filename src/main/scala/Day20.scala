546a21f3c07b2944b20efe19a6ecd6d4fa02497b
Richard Holmes <richard.gordon.holmes@gmail.com>
2022-12-20 17:40:21 +0000

    Simplify day 20

--- src/main/scala/Day20.scala
+++ src/main/scala/Day20.scala
@@ -1,5 +1,5 @@
 object Day20 extends IDay {
-  override def execute(input: String): (Any, Any) = {
+  override def execute(input: String): (Long, Long) = {
     val sequence: Array[Long] = Helper.readLinesInt(input).map(_.toLong).toArray
     (part1(sequence), part2(sequence))
   }
@@ -13,9 +13,7 @@ object Day20 extends IDay {
       (if (x >= 0) x else x + divisor).toInt
     }
 
-    def negModN(index: Long): Int = {
-      negMod(index, n)
-    }
+    def negModN(index: Long): Int = negMod(index, n)
 
     def currentIndex(originalIndex: Int): Int = negModN(originalIndex + changes(originalIndex))
 
@@ -32,61 +30,63 @@ object Day20 extends IDay {
       val originalIndex: Int = pair._2
 
       val startIndex: Int = currentIndex(originalIndex)
-      val targetIndex: Int = negModN(startIndex + sequenceValue)
 
-      def determineNewChanges: Array[Long] = {
+      def determineNewChanges: ChangeStruct = {
         val allMove: Long = -sequenceValue / (n - 1)
         val someMove: Long = allMove + (if (sequenceValue <= 0) 1 else -1)
         val extraMoveCount: Int = negMod(sequenceValue.abs, n - 1)
-        if (sequenceValue >= 0) {
-          val rightExtra: Int = Math.min(n - startIndex - 1, extraMoveCount)
-          val leftExtra: Int = Math.max(0, extraMoveCount - rightExtra)
-          Array.fill(leftExtra)(someMove) ++ Array.fill(startIndex - leftExtra)(allMove) ++ Array(sequenceValue) ++
-            Array.fill(rightExtra)(someMove) ++ Array.fill(n - startIndex - rightExtra - 1)(allMove)
-        } else {
-          val leftExtra: Int = Math.min(startIndex, extraMoveCount)
-          val rightExtra: Int = Math.max(0, extraMoveCount - leftExtra)
-          Array.fill(startIndex - leftExtra)(allMove) ++ Array.fill(leftExtra)(someMove) ++ Array(sequenceValue) ++
-            Array.fill(n - startIndex - rightExtra - 1)(allMove) ++ Array.fill(rightExtra)(someMove)
-        }
+        new ChangeStruct(sequenceValue, allMove, someMove, extraMoveCount, startIndex, n)
       }
 
-      val newChanges: Array[Long] = determineNewChanges
+      val newChanges: ChangeStruct = determineNewChanges
 
-      if (newChanges.sum != 0)
-        sys.error("new changes sum nonzero, actual sum " + newChanges.sum)
-      if (newChanges.length != n)
-        sys.error("new changes incorrect length")
+      def newChangesOriginalValue(index: Int): Long = newChanges.getAtIndex(negModN(currentIndex(index)))
 
-      def newChangesUnshuffleValue(index: Int): Long = {
-        newChanges(negModN(currentIndex(index)))
-      }
-
-      val unshuffledNewChanges: Array[Long] = (0 until n).map(newChangesUnshuffleValue).toArray
-
-      changes = changes.zip(unshuffledNewChanges).map(p => p._1 + p._2)
+      changes = changes.zip((0 until n).map(newChangesOriginalValue)).map(p => p._1 + p._2)
     }
 
-    (0 until repeats).foreach({n =>
-      originalSequence.zipWithIndex.foreach(processMove)
-    })
+    (0 until repeats).foreach(_ => originalSequence.zipWithIndex.foreach(processMove))
     reconstructSequence
   }
 
-  def part1(originalSequence: Array[Long]): Long = {
-    val sequence: Array[Long] = mixSequence(originalSequence, 1)
-    if (sequence.length != originalSequence.length)
-      sys.error("mismatched sequence length")
+  def groveCoordinates(sequence: Array[Long]): Long = {
     val zeroIndex: Int = sequence.zipWithIndex.find(_._1 == 0).get._2
     (1 to 3).map(n => sequence((1000 * n + zeroIndex) % sequence.length)).sum
   }
+
+  def part1(originalSequence: Array[Long]): Long = groveCoordinates(mixSequence(originalSequence, 1))
 
   def part2(originalSequence: Array[Long]): Long = {
     val decryptionKey: Long = 811589153
-    val sequence: Array[Long] = mixSequence(originalSequence.map(_ * decryptionKey), 10)
-
-    val zeroIndex: Int = sequence.zipWithIndex.find(_._1 == 0).get._2
-    (1 to 3).map(n => sequence((1000 * n + zeroIndex) % sequence.length)).sum
+    val decryptSequence: Array[Long] = originalSequence.map(_ * decryptionKey)
+    groveCoordinates(mixSequence(decryptSequence, 10))
   }
 
+
+  class ChangeStruct(val sequenceValue: Long, val allMove: Long, val someMove: Long, val extraMoveCount: Int,
+                     val startIndex: Int, val n: Int) {
+    def getAtIndex(i: Int): Long = if (sequenceValue >= 0) getAtIndexPositive(i) else getAtIndexNegative(i)
+
+    def getAtIndexPositive(i: Int): Long = {
+      val rightExtra: Int = Math.min(n - startIndex - 1, extraMoveCount)
+      val leftExtra: Int = Math.max(0, extraMoveCount - rightExtra)
+
+      if (i < leftExtra) someMove
+      else if (i < startIndex) allMove
+      else if (i == startIndex) sequenceValue
+      else if (i > startIndex + rightExtra) allMove
+      else someMove
+    }
+
+    def getAtIndexNegative(i: Int): Long = {
+      val leftExtra: Int = Math.min(startIndex, extraMoveCount)
+      val rightExtra: Int = Math.max(0, extraMoveCount - leftExtra)
+
+      if (i < startIndex - leftExtra) allMove
+      else if (i < startIndex) someMove
+      else if (i == startIndex) sequenceValue
+      else if (i >= n - rightExtra) someMove
+      else allMove
+    }
+  }
 }
