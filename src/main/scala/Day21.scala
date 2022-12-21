c98d21cd535e0c32533dc7a2010111b1dc580ce5
Richard Holmes <richard.gordon.holmes@gmail.com>
2022-12-21 14:30:14 +0000

    Complete day 21

--- src/main/scala/Day21.scala
+++ src/main/scala/Day21.scala
@@ -1,213 +1,158 @@
 object Day21 extends IDay {
-  type Monkeys = Iterable[Monkey]
-
-  override def execute(input: String): (Any, Any) = {
-    // val input = "root: pppw + sjmn\ndbpl: 5\ncczh: sllz + lgvd\nzczc: 2\nptdq: humn - dvpt\ndvpt: 3\nlfqf: 4\nhumn: 5\nljgn: 2\nsjmn: drzm * dbpl\nsllz: 4\npppw: cczh / lfqf\nlgvd: ljgn * ptdq\ndrzm: hmdt - zczc\nhmdt: 32"
-    val monkeys: Monkeys = Helper.readLines(input, Monkey.read)
-    (part1(monkeys), incomplete) // part2(monkeys))
-  }
-
   val ROOT_NAME: String = "root"
   val HUMAN_NAME: String = "humn"
 
-  def part1(monkeys: Monkeys): Long = {
-    monkeys.find(_.name == ROOT_NAME).get.getResult(monkeys)
+  override def execute(input: String): (Long, Long) = {
+    (part1(Monkey.readMonkeyList(input, withHuman = false)), part2(Monkey.readMonkeyList(input, withHuman = true)))
   }
 
-  def part2(monkeys: Monkeys): Long = {
-    val originalRootMonkey: OpMonkey = monkeys.find(_.name == ROOT_NAME).get.asInstanceOf[OpMonkey]
-    val humanMonkey: HumanMonkey = HumanMonkey()
-    val rootMonkey: RootMonkey = RootMonkey(originalRootMonkey.left, originalRootMonkey.right)
-    val newMonkeys: Monkeys = humanMonkey :: monkeys.filter(monkey => monkey.name != ROOT_NAME && monkey.name != HUMAN_NAME).toList
+  def part1(rootMonkey: Monkey): Long = rootMonkey.getResult
 
-    val leftEquation: Long => Long = rootMonkey.findLeft(newMonkeys).getHumanEquation(newMonkeys)
-    val rightEquation: Long => Long = rootMonkey.findRight(newMonkeys).getHumanEquation(newMonkeys)
+  def part2(rootMonkey: Monkey): Long = {
+    val originalRootMonkey: OpMonkey = rootMonkey match {
+      case monkey: OpMonkey => monkey
+    }
+    var root: RootMonkey = if (originalRootMonkey.left.dependsOnHuman)
+      RootMonkey(originalRootMonkey.left, originalRootMonkey.right)
+    else
+      RootMonkey(originalRootMonkey.right, originalRootMonkey.left)
 
-    var x: Long = 0
-    while (leftEquation(x) != rightEquation(x)) x += 1
-    x
+    while (!root.left.isHuman) {
+      val (newLeft, newRight) = root.left match {
+        case monkey: OpMonkey => monkey.simplifyHumanBranch(root.right)
+      }
+      root = RootMonkey(newLeft, newRight)
+    }
+    root.right.getResult
   }
 
   abstract class Monkey(val name: String) {
-    def getResult(monkeys: Monkeys): Long
+    def getResult: Long
 
-    def getConstraint(monkeys: Monkeys): Constraint
+    def dependsOnHuman: Boolean
 
-    def getHumanEquation(monkeys: Monkeys): Long => Long
+    def isHuman: Boolean = false
+
+    def toEquationString: String
+
+    def simplify: Monkey
   }
 
   case class NumberMonkey(override val name: String, number: Long) extends Monkey(name) {
-    override def getResult(monkeys: Monkeys): Long = number
+    override def getResult: Long = number
 
-    override def getConstraint(monkeys: Monkeys): Constraint = ???
+    override def dependsOnHuman: Boolean = false
 
-    override def getHumanEquation(monkeys: Monkeys): Long => Long = _ => number
+    override def toEquationString: String = s"$number"
+
+    override def simplify: Monkey = this
   }
 
-  case class OpMonkey(override val name: String, left: String, right: String, op: (Long, Long) => Long) extends Monkey(name) {
-    var result: Option[Long] = None
+  case class RootMonkey(left: Monkey, right: Monkey) extends Monkey(ROOT_NAME) {
+    override def getResult: Long = sys.error("should not evaluate root monkey")
 
-    var humanEquation: Option[Long => Long] = None
+    override def dependsOnHuman: Boolean = true
 
-    override def getResult(monkeys: Monkeys): Long = {
-      if (result.isEmpty) result = Some(op(findLeft(monkeys).getResult(monkeys), findRight(monkeys).getResult(monkeys)))
-      result.get
-    }
+    override def toEquationString: String = s"(${left.toEquationString} === ${right.toEquationString})"
 
-    def findLeft(monkeys: Monkeys): Monkey = monkeys.find(_.name == left).get
-
-    def findRight(monkeys: Monkeys): Monkey = monkeys.find(_.name == right).get
-
-    override def getConstraint(monkeys: Monkeys): Constraint = ???
-
-    override def getHumanEquation(monkeys: Monkeys): Long => Long = {
-      if (humanEquation.isEmpty)
-        humanEquation = Some({
-          x => repurposeOp(findLeft(monkeys).getHumanEquation(monkeys), findRight(monkeys).getHumanEquation(monkeys))(x)
-        })
-      humanEquation.get
-    }
-
-    def repurposeOp(leftOp: Long => Long, rightOp: Long => Long): Long => Long = {
-       x => op(leftOp(x), rightOp(x))
-    }
-  }
-
-  case class RootMonkey(left: String, right: String) extends Monkey(ROOT_NAME) {
-    override def getResult(monkeys: Monkeys): Long = ???
-
-    override def getConstraint(monkeys: Monkeys): Constraint = ???
-
-    override def getHumanEquation(monkeys: Monkeys): Long => Long = ???
-
-    def findLeft(monkeys: Monkeys): Monkey = monkeys.find(_.name == left).get
-
-    def findRight(monkeys: Monkeys): Monkey = monkeys.find(_.name == right).get
+    override def simplify: Monkey = RootMonkey(left.simplify, right.simplify)
   }
 
   case class HumanMonkey() extends Monkey(HUMAN_NAME) {
-    override def getResult(monkeys: Monkeys): Long = ???
+    override def getResult: Long = sys.error("human value is unknown")
 
-    override def getConstraint(monkeys: Monkeys): Constraint = ???
+    override def dependsOnHuman: Boolean = true
 
-    override def getHumanEquation(monkeys: Monkeys): Long => Long = x => x
+    override def isHuman: Boolean = true
+
+    override def toEquationString: String = "x"
+
+    override def simplify: Monkey = this
   }
+
+  abstract class OpMonkey(override val name: String, val left: Monkey, val right: Monkey) extends Monkey(name) {
+    override def getResult: Long = {
+      getOp(left.getResult, right.getResult)
+    }
+
+    override def dependsOnHuman: Boolean = left.dependsOnHuman || right.dependsOnHuman
+
+    def getOp: (Long, Long) => Long = this match {
+      case _: AddMonkey => (l, r) => l + r
+      case _: SubMonkey => (l, r) => l - r
+      case _: MulMonkey => (l, r) => l * r
+      case _: DivMonkey => (l, r) => l / r
+    }
+
+    def simplifyHumanBranch(otherBranch: Monkey): (Monkey, Monkey) = (left.dependsOnHuman, this) match {
+      case (true, AddMonkey(name, left, right)) => (left, SubMonkey(name + "-", otherBranch, right))
+      case (false, AddMonkey(name, left, right)) => (right, SubMonkey(name + "-", otherBranch, left))
+      case (true, SubMonkey(name, left, right)) => (left, AddMonkey(name + "+", otherBranch, right))
+      case (false, SubMonkey(name, left, right)) => (right, SubMonkey(name + "-", left, otherBranch))
+      case (true, MulMonkey(name, left, right)) => (left, DivMonkey(name + "/", otherBranch, right))
+      case (false, MulMonkey(name, left, right)) => (right, DivMonkey(name + "/", otherBranch, left))
+      case (true, DivMonkey(name, left, right)) => (left, MulMonkey(name + "*", right, otherBranch))
+      case (false, DivMonkey(name, left, right)) => (right, DivMonkey(name + "/", left, otherBranch))
+    }
+
+    override def simplify: Monkey = {
+      if (!dependsOnHuman) NumberMonkey(name, getResult)
+      else this match {
+        case AddMonkey(name, left, right) => AddMonkey(name, left.simplify, right.simplify)
+        case SubMonkey(name, left, right) => SubMonkey(name, left.simplify, right.simplify)
+        case MulMonkey(name, left, right) => MulMonkey(name, left.simplify, right.simplify)
+        case DivMonkey(name, left, right) => DivMonkey(name, left.simplify, right.simplify)
+      }
+    }
+
+    override def toEquationString: String = s"(${left.toEquationString} $opChar ${right.toEquationString})"
+
+    def opChar: Char = this match {
+      case _: AddMonkey => '+'
+      case _: SubMonkey => '-'
+      case _: MulMonkey => '*'
+      case _: DivMonkey => '/'
+    }
+  }
+
+  case class AddMonkey(override val name: String, override val left: Monkey, override val right: Monkey)
+    extends OpMonkey(name, left, right)
+
+  case class SubMonkey(override val name: String, override val left: Monkey, override val right: Monkey)
+    extends OpMonkey(name, left, right)
+
+  case class MulMonkey(override val name: String, override val left: Monkey, override val right: Monkey)
+    extends OpMonkey(name, left, right)
+
+  case class DivMonkey(override val name: String, override val left: Monkey, override val right: Monkey)
+    extends OpMonkey(name, left, right)
+
 
   object Monkey {
-    def read(line: String): Monkey = {
-      val numberPattern = "(\\w{4}): (\\d+)".r
-      val opPattern = "(\\w{4}): (\\w{4}) ([+\\-*/]) (\\w{4})".r
+    def readMonkeyList(input: String, withHuman: Boolean): Monkey = {
+      var lines: Map[String, String] = Map()
+      input.split("\\r?\\n").foreach(str => lines += str.substring(0, 4) -> str)
 
-      def readOp(op: Char): (Long, Long) => Long = op match {
-        case '+' => (l, r) => l + r
-        case '-' => (l, r) => l - r
-        case '*' => (l, r) => l * r
-        case '/' => (l, r) => l / r
+      def readOpMonkey(name: String, left: Monkey, right: Monkey, op: Char): OpMonkey = op match {
+        case '+' => AddMonkey(name, left, right)
+        case '-' => SubMonkey(name, left, right)
+        case '*' => MulMonkey(name, left, right)
+        case '/' => DivMonkey(name, left, right)
       }
 
-      line match {
-        case numberPattern(name, number) => NumberMonkey(name, number.toInt)
-        case opPattern(name, left, op, right) => OpMonkey(name, left, right, readOp(op.head))
+      def getMonkey(name: String): Monkey = {
+        if (withHuman && name == HUMAN_NAME) return HumanMonkey()
+
+        val numberPattern = "(\\w{4}): (\\d+)".r
+        val opPattern = "(\\w{4}): (\\w{4}) ([+\\-*/]) (\\w{4})".r
+
+        lines(name) match {
+          case numberPattern(name, number) => NumberMonkey(name, number.toInt)
+          case opPattern(name, left, op, right) => readOpMonkey(name, getMonkey(left), getMonkey(right), op.head)
+        }
       }
+
+      getMonkey("root").simplify
     }
-  }
-
-  abstract class Constraint() {
-    def isSatisfied(monkeys: Monkeys, number: Long): Boolean
-  }
-
-  case class EqConstraint(left: Monkey, right: Monkey) extends Constraint {
-    override def isSatisfied(monkeys: Monkeys, number: Long): Boolean = {
-      left.getResult(monkeys) == right.getResult(monkeys)
-    }
-  }
-
-  case class NoneConstraint() extends Constraint {
-    override def isSatisfied(monkeys: Monkeys, number: Long): Boolean = true
-  }
-
-  case class OpConstraint(left: Constraint, right: Constraint, op: (Long, Long) => Long) extends Constraint {
-    override def isSatisfied(monkeys: Monkeys, number: Long): Boolean =
-      left.isSatisfied(monkeys, number) && right.isSatisfied(monkeys, number)
-  }
-
-  abstract class Expr {
-    var dependsOnHumanResult: Option[Boolean] = None
-    var evalResult: Option[Long] = None
-
-    def eval(humanValue: Long): Long = {
-      if (dependsOnHuman) evalOperation(humanValue)
-      else {
-        if (evalResult.isEmpty) evalResult = Some(evalOperation(humanValue))
-        evalResult.get
-      }
-    }
-
-    def dependsOnHuman: Boolean = {
-      if (dependsOnHumanResult.isEmpty) dependsOnHumanResult = Some(checkDependsOnHuman)
-      dependsOnHumanResult.get
-    }
-
-    def evalOperation(humanValue: Long): Long
-
-    def reverse: Expr
-
-    def checkDependsOnHuman: Boolean
-  }
-
-  case class Num(n: Long) extends Expr {
-    override def evalOperation(humanValue: Long): Long = n
-
-    override def reverse: Expr = Num(n)
-
-    override def checkDependsOnHuman: Boolean = false
-  }
-
-  case class Add(l: Expr, r: Expr) extends Expr {
-    override def evalOperation(humanValue: Long): Long = l.evalOperation(humanValue) + r.evalOperation(humanValue)
-
-    override def reverse: Expr = Sub(l, r)
-
-    override def checkDependsOnHuman: Boolean = l.dependsOnHuman || r.dependsOnHuman
-  }
-
-  case class Sub(l: Expr, r: Expr) extends Expr {
-    override def evalOperation(humanValue: Long): Long = l.evalOperation(humanValue) - r.evalOperation(humanValue)
-
-    override def reverse: Expr = Add(l, r)
-
-    override def checkDependsOnHuman: Boolean = l.dependsOnHuman || r.dependsOnHuman
-  }
-
-  case class Mult(l: Expr, r: Expr) extends Expr {
-    override def evalOperation(humanValue: Long): Long = l.evalOperation(humanValue) * r.evalOperation(humanValue)
-
-    override def reverse: Expr = Div(l, r)
-
-    override def checkDependsOnHuman: Boolean = l.dependsOnHuman || r.dependsOnHuman
-  }
-
-  case class Div(l: Expr, r: Expr) extends Expr {
-    override def evalOperation(humanValue: Long): Long = l.evalOperation(humanValue) / r.evalOperation(humanValue)
-
-    override def reverse: Expr = Mult(l, r)
-
-    override def checkDependsOnHuman: Boolean = l.dependsOnHuman || r.dependsOnHuman
-  }
-
-  case class HumanVar() extends Expr {
-    override def evalOperation(humanValue: Long): Long = humanValue
-
-    override def reverse: Expr = HumanVar()
-
-    override def checkDependsOnHuman: Boolean = true
-  }
-
-  case class MonkeyVar(name: String) extends Expr {
-    override def evalOperation(humanValue: Long): Long = ???
-
-    override def reverse: Expr = MonkeyVar(name)
-
-    override def checkDependsOnHuman: Boolean = false
   }
 }
